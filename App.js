1ce7e5d53278fe824ab018b1e864cabcc667bb81
Liantsoa123 <rakotonanaharyliantsoafan@gmail.com>
2024-10-24 19:48:01 +0300

    add body component

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import Welcome from './screens/Welcome';
 import ClassComponents from './screens/ClassComponents';
+import Body from './screens/Body';
 
 export default function App() {
 
-  const [count, setCount] = useState(0);
 
   return (
-    <View>
+    <View style={styles.container} >
       <Welcome name={'Liantsoa'} />
+      <Body />
       <ClassComponents name="Fanantenana" />
     </View>
   );
 
 }
+
+styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: '#f0f0f0',
+    justifyContent: 'center',
+  }
+});
 
 
--- screens/Body.js
+++ screens/Body.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 
 const Body = () => {
+    const [count, setCount] = useState(0);
     return (
         <View style={styles.container} >
             <Text style={styles.text}>You clicked {count} times</Text>
@@ -10,12 +11,4 @@ const Body = () => {
         </View>
     )
 }
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-
-});
+export default Body; 
